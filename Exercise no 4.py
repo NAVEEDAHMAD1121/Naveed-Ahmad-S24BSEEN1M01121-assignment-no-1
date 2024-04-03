ac78042bddf7c67c3c8ae4ff44b2e72dbfb2c7d0
NAVEEDAHMAD1121 <165829776+NAVEEDAHMAD1121@users.noreply.github.com>
2024-04-03 10:38:20 -0700

    Update Exercise no 4.py

--- Exercise no 4.py
+++ Exercise no 4.py
@@ -1,5 +1,6 @@
-#AttributeError: 'str' object has no attribute 'remove'
-chars=("physiology")
-n=6
-new=chars[n:]
-print(new)
+string=input("enter any string")
+num=int(input("enter the num"))
+if num < len(string):
+    print(string[num:])
+else:
+    print("num must less than the length of the string")
